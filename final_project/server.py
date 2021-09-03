c4824dea27016b2e38882332dc4284746568d48f
magnus ziegler <magnus.c.ziegler@gmail.com>
2021-09-03 15:42:45 +0000

    fix

--- final_project/server.py
+++ final_project/server.py
@@ -8,13 +8,13 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return machinetranslation.english_to_french(textToTranslate)
+    return translator.english_to_french(textToTranslate)
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return machinetranslation.french_to_english(textToTranslate)
+    return translator.french_to_english(textToTranslate)
 
 @app.route("/")
 def renderIndexPage():
